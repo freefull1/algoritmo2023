c3cc4f8ad4980aff2e66b0b8c3b09530a9535209
freefull1 <maxi_mmi@hotmail.com>
2023-03-27 22:02:11 -0300

    Es uno de los ejercicios de la guia

--- ejercicios.py
+++ ejercicios.py
@@ -1,4 +1,11 @@
-asdasdasd
-asdas
-dasd
-as
+def potencia(a, b):
+    # Caso base: si el exponente es 0, la potencia es 1
+    if b == 0:
+        return 1
+    # Caso recursivo: calculamos la potencia de a^(b-1) y lo multiplicamos por a
+    else:
+        return a * potencia(a, b-1)
+
+
+
+print(potencia(2,2))
